04eb060df811cc9439547055046f8377446b723a
logan <444193+brainsnail@users.noreply.github.com>
2021-09-04 13:05:10 -0700

    Chapter 1: code suggestions from clippy applied

--- clippy/src/main.rs
+++ clippy/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let MYLIST = ["One", "Two", "Three"];
-    for i in 0..3 {
-        println!("{}", MYLIST[i]);
+    // "Turning Clippy up to pedantic can feel like an exercise in masochism, but it’s a great way to improve your code."
+    // ^ I love this quote
+    let my_list = ["One", "Two", "Three"];
+    for item in &my_list {
+        println!("{}", item);
     }
 }
